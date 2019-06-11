de99b25226ee06cd19153afea66c7d4949c497d3
The Hurt Doctor <akacrasher@akacrasher.com>
2019-06-11 14:32:50 -0600

    updates

--- ex32.py
+++ ex32.py
@@ -15,15 +15,14 @@ for fruit in fruits:
 for i in change:
     print(f"I got {i}")
 
-# we can also build empty lists like this
-elements = []
-
 # then use the range function to do 0 to 5 counts
-for i in range(0, 6):
-    print(f"Adding {i} to the list.")
-    # append is a function that lists understand
-    elements.append(i)
+#for i in range(0, 6):
+#    print(f"Adding {i} to the list.")
+#    # append is a function that lists understand
+#    elements.append(i)
+
+# A better way to build the list instead of the above for loop
+elements = list(range(6))
 
 for i in elements:
     print(f"Element was: {i}")
-    
