85136a7bd0bb6ef9f299fc02d134eb83e82faa2b
The Hurt Doctor <akacrasher@akacrasher.com>
2019-06-11 15:26:49 -0600

    rewrite to use function

--- ex33.py
+++ ex33.py
@@ -1,16 +1,24 @@
-i = 0
+print("Enter total numbers: ")
+tn = int(input("> "))
+print("Enter increment value: ")
+iv = int(input("> "))
+
 numbers = list()
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+def listAppend(totalnums, incr=1):
+    """ Appends totalnums numbers to the end of the numbers list
+    using incr increment """
+    i = 0
+    while i < totalnums:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-    i = i + 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+        i = i + incr
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
 
+listAppend(tn, iv)
 
 print("The numbers: ")
-
 for num in numbers:
     print(num)
